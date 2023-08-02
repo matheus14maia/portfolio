57749350eadf3ebb9af151fb9583ae87f71d1dc6
Matheus Maia <matheusmaia@ficusconsultoria.com.br>
2023-08-02 18:14:21 -0300

    remover svg e comentando linhas inuteis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import About from './components/About';
-import Contact from './components/Contact';
+//import Contact from './components/Contact';
 import Navbar from './components/Navbar';
-import Projects from './components/Projects';
+//import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 function App() {
